8055d36b7b921f6fd8b8bb061a16e1059cd674e8
joshroundy9 <joshroundy9@gmail.com>
2024-07-20 21:34:29 -0500

    [Cherry-MASTER]

---
+++ src/test/java/com/joshroundy/cherry/CherryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshroundy.cherry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
