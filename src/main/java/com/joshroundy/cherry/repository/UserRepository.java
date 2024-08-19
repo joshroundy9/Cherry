9bc67f5a76e1fab805470f67d44487ea0d00674b
joshroundy9 <joshroundy9@gmail.com>
2024-08-18 22:14:29 -0400

    Create directory structure and connect to SQL Server.

---
+++ src/main/java/com/joshroundy/cherry/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.joshroundy.cherry.repository;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+}
