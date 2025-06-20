30f5f50af5740490b7494b0ff5f0885cc5111760
joshroundy9 <104024433+joshroundy9@users.noreply.github.com>
2025-06-20 17:42:52 -0500

    install: react router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Link className="App-link" to="/login">
+              Go to Login
+            </Link>
+          </header>
+          <Routes>
+            <Route path="/login" element={<Login onLogin={() => {}} />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
