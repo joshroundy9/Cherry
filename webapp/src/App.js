ace72896f530af8b747d27ace6f18b98d269f514
joshroundy9 <104024433+joshroundy9@users.noreply.github.com>
2025-06-20 18:33:51 -0500

    add: routing and basic app header

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,3 +1,4 @@
+// webapp/src/App.js
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
@@ -7,18 +8,27 @@ function App() {
   return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <Link className="App-link" to="/login">
-              Go to Login
-            </Link>
-          </header>
-          <Routes>
-            <Route path="/login" element={<Login onLogin={() => {}} />} />
-          </Routes>
+            <header className="Main-header">
+                <Link className="Logo-text" to="/">
+                    <h1>CHERRY</h1>
+                    <img src="/logo56.png" alt="Logo"/>
+                </Link>
+            </header>
+            <Routes>
+              <Route path="/" element={
+                  <div className="App-header">
+                      <img src={logo} className="App-logo" alt="logo"/>
+                      <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                      </p>
+                      <Link className="App-link" to="/login">
+                        Go to Login
+                      </Link>
+                  </div>
+              }/>
+              <Route path="/login" element={<Login onLogin={() => {
+              }}/>}/>
+            </Routes>
         </div>
       </Router>
   );
