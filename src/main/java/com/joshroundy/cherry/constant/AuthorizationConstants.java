b2cd98eda18842cb4aea0c0096aa43ddee2234f2
joshroundy9 <104024433+joshroundy9@users.noreply.github.com>
2025-08-04 21:17:56 -0500

    increase: userrname length to 18 chars

--- src/main/java/com/joshroundy/cherry/constant/AuthorizationConstants.java
+++ src/main/java/com/joshroundy/cherry/constant/AuthorizationConstants.java
@@ -1,8 +1,8 @@
 package com.joshroundy.cherry.constant;
 
 public class AuthorizationConstants {
-    public static final String USERNAME_REGEX = "^[a-zA-Z0-9]{3,12}$";
-    public static final String USERNAME_ERROR_MESSAGE = "Username must be alphanumeric and less than 12 characters";
+    public static final String USERNAME_REGEX = "^[a-zA-Z0-9]{3,18}$";
+    public static final String USERNAME_ERROR_MESSAGE = "Username must be alphanumeric and less than 18 characters";
     public static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
     public static final String EMAIL_ERROR_MESSAGE = "Email must be a valid email address";
     public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$";
